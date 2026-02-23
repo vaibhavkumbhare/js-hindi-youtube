e3a03f8ef26f9013be8034ba99823521ce7540fd
vaibhavkumbhare <vaibhav.kumbhare45@gmail.com>
2026-02-23 13:49:56 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = true 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase => 0
+
+let isLoggedIn = "2"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> flase
+// "" => flase
+// "vaibhav" => true
+
+let someNumber ="vaibhav"
+
+let stringNumber =String(someNumber)
+console.log(someNumber)
