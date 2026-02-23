a71b9b0ee1721c8ecde7b4816224129b851aec19
vaibhavkumbhare <vaibhav.kumbhare45@gmail.com>
2026-02-23 12:42:35 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "vaibhav.kumbhare45@gmail.com"
+var accountpassword = "12345"
+acctountCity = "Ahmadabad"
+let accountState;
+
+accountEmail ="hc@hc.com"
+accountpassword = "21212121"
+accountCity = "Bengaluru"
+
+// accountId =2 // Not allowed
+console.log(accountId);
+
+/*
+prefer not to use var
+beacause of issue in black scope and function scope 
+*/
+
+console.table([accountId, accountEmail, accountpassword ,accountCity, accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3 + 3)
+console.log("vaibhav")
+
+let name ="vaibhav"
+let age = 18
+let isloggedIn = false
+
+// number => 2 to power 53
+// biging  => true/false
+// null => standalone value
+// underfined => 
+// symbol => unique
+
+console.log(typeof age);
