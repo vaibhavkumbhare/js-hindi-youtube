88c17e0252281d9d3523b6909d2b555ca736d3bd
vaibhavkumbhare <vaibhav.kumbhare45@gmail.com>
2026-02-24 13:55:20 +0000

    change

--- 01_basics/04_comparition.js
+++ 01_basics/04_comparition.js
@@ -9,10 +9,42 @@
 // console.log(null <= 0);
 // console.log(null == 0);
 
-console.log(undefined == 0);
-console.log(undefined < 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined < 0);
 
 
 
 // console.log("2" === 2)   //  chak karna variable hai ki nahi hai 
+
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++///////////+++++++++++++++++++++++++++++++++++++++++
+ 
+
+// Stack (Primitive), Heap (Not-primitive)
+
+let myYoutubename = "vaibhav@google.com"
+
+// let anothername = myYoutubename
+let anothername = "Youtube"
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+
+
+
+// Heap (Not-primitive)
+
+let userOne = {
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+let userTwo = userOne
+userTwo.email = "vaibhav.gmail.com"
+
+console.log(userOne.email);
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "vaaibhav"
+const repoCount = 50
+
+// console.log(name + repoCount +" Value");
+// console.log(`Hello my name is  ${name} and my repoCount is ${repoCount}`);
+
+
+const gameName = new String('Hitesh-hc')
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2))
+console.log(gameName.indexOff)
+
