b1b2a5c43690eb3715da3eefcaebd63a42a744d2
vaibhavkumbhare <vaibhav.kumbhare45@gmail.com>
2026-02-25 13:36:43 +0000

    data

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,27 @@
+// Dates
+
+// let myDate = new Date ()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2026, 1, 25 )
+// let myCreatedDate = new Date(2026, 1, 25, 5, 3 )
+// let myCreatedDate = new Date("2026-01-25")
+let myCreatedDate = new Date("02-25-2026")
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000)) 
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDate());
+console.log(newDate.getTime());
