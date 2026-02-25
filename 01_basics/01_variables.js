dcd88d2e777b7d0ed21056d8aa542cd69db26140
vaibhavkumbhare <vaibhav.kumbhare45@gmail.com>
2026-02-25 09:03:45 +0000

    05_strings.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -9,12 +9,12 @@ accountpassword = "21212121"
 accountCity = "Bengaluru"
 
 // accountId =2 // Not allowed
-console.log(accountId);
+console.log(acctountCity);
 
 /*
 prefer not to use var
 beacause of issue in black scope and function scope 
 */
 
-console.table([accountId, accountEmail, accountpassword ,accountCity, accountState])
+// console.table([accountId, accountEmail, accountpassword ,accountCity, accountState])
 
--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,8 +1,8 @@
-const name = "vaaibhav"
+const name = "vaibhav"
 const repoCount = 50
 
-// console.log(name + repoCount +" Value");
-// console.log(`Hello my name is  ${name} and my repoCount is ${repoCount}`);
+// console.log(name + repoCount +" Value"); // not use 
+// console.log(`Hello my name is  ${name} and my repoCount is ${repoCount}`);  // use this always
 
 
 const gameName = new String('Hitesh-hc')
@@ -12,5 +12,21 @@ const gameName = new String('Hitesh-hc')
 // console.log(gameName.length)
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(2))
-console.log(gameName.indexOff)
+// console.log(gameName.indexOf('t'));
+console.log(gameName.split('-'))
 
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+const newStringOne = "  hitesh  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://vaibhav.com/vaibhav%20kumbhare"
+
+console.log(url.replace('%20', '_'))
+
+console.log(url.includes('.com'));
