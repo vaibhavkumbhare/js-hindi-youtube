e165f9a1914b167aa48053d5e1e58e797cbea6df
vaibhavkumbhare <vaibhav.kumbhare45@gmail.com>
2026-02-27 13:00:30 +0000

    functin

---
+++ 03_function.js/01_function.js
@@ -0,0 +1,25 @@
+
+
+function sayMyname(){
+    console.log("v")
+    console.log("a")
+    console.log("i")    
+    console.log("b")
+    console.log("h")
+    console.log("a")
+    console.log("v")
+}
+// sayMyname()
+
+// function addTwoNumber(number1,number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumber(number1,number2){
+    let result = number1 + number2
+    console.log("Vaibhav");
+    return result
+}
+const result = addTwoNumber(3 ,5) 
+
+console.log("Result: ", result);
