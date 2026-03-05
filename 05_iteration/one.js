f8879f100de13550ac4d927b19ae1b5319ae4c23
vaibhavkumbhare <vaibhav.kumbhare45@gmail.com>
2026-03-05 07:06:29 +0000

    one.js

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -8,10 +8,37 @@
 //     console.log(element)
 // }
 
-for (let i = 1; i <= 10; i++) {
-    console.log(`Outer loop value ${i}`)
-for (let j = 1; j <= 10; j++) {
-    // console.log(`Innear loop value ${j} and inner loop ${i}`)
-    console.log(i + '*' + j + '=' + i*j)
-}    
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value ${i}`)
+// for (let j = 1; j <= 10; j++) {
+//     // console.log(`Innear loop value ${j} and inner loop ${i}`)
+//     console.log(i + '*' + j + '=' + i*j)
+// }    
+// }
+
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length)
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+// break and continue
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log("Detected 5")
+//         break;
+//     }
+//     console.log(`Vailue of i is ${index}`);
+// }
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log("Detected 5")
+        continue;
+    }
+    console.log(`Vailue of i is ${index}`);
 }
